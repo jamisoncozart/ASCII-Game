c2fbefebadfd9ab808cf630c3dbf50d147bea742
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-26 20:49:00 -0800

    Add player movement left and right, update gameBoard to actively display player movement

--- main.cs
+++ main.cs
@@ -11,20 +11,33 @@ public class Program
     Console.Clear();
     newGame.CreateGame();
     int[][] gameBoard = newGame.GetGame();
-
-    for (int i = 0; ; i++)
+    bool paused = false;
+    while (true)
     {
+      if (Console.KeyAvailable)
+      {
+        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+        if (keyInfo.Key == ConsoleKey.RightArrow) newPlayer.MovePlayer("right");
+        else if (keyInfo.Key == ConsoleKey.LeftArrow) newPlayer.MovePlayer("left");
+        else break;
+      }
       Console.SetCursorPosition(Console.WindowLeft, Console.WindowTop);
-      Console.Write(GetGameBoardString(gameBoard));
+      Console.Write(GetGameBoardString(gameBoard)); 
     }
+    // for (int i = 0; ; i++)
+    // {
+    //   Console.SetCursorPosition(Console.WindowLeft, Console.WindowTop);
+    //   Console.Write(GetGameBoardString(gameBoard));
+    // }
   }
 
   public static string GetGameBoardString(int[][] gameBoard)
   {
     //add player to board
+    newGame.CreateGame();
     int playerX = newPlayer.getPlayerX();
     int playerY = newPlayer.getPlayerY();
-    gameBoard[playerX][playerY] = 2;
+    gameBoard[playerY][playerX] = 2;
     //create board string
     string gameBoardString = "";
     for(int i = 0; i < gameBoard.Length; i++)
@@ -37,5 +50,6 @@ public class Program
   public static void movePlayerController()
   {
     //move player based on key strokes
+
   }
 }
